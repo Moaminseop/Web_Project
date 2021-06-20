ab71ed86e805a95c9cdcbaf15a60677e18e30cd3
Min Seop Song <sms971113@gmail.com>
2021-06-20 16:27:58 +0900

    create formerrors and login design

--- SEproject/Shopping_Mall/accounts/urls.py
+++ SEproject/Shopping_Mall/accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
